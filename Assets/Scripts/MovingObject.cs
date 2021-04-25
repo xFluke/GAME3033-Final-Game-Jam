f07c3315cc2709b1f7a2aba5f860ca49f3145fc1
xFluke <mikoman1124@gmail.com>
2021-04-25 02:25:39 -0400

    Destroy moving game objects when get too far

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -6,7 +6,14 @@ public class MovingObject : MonoBehaviour
 {
     [SerializeField] float speed;
 
+    float timeAlive = 0f;
     private void Update() {
+        if (timeAlive > 8f) {
+            Destroy(gameObject);
+        }
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        timeAlive += Time.deltaTime;
     }
 }
